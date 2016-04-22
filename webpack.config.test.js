7b778d6a7f85f927a9cf1deced7505da5f091bd3
ktsn <ktsn55@gmail.com>
2016-04-23 01:24:57 +0900

    Fix build configurations

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -2,23 +2,19 @@
 const path = require('path');
 const glob = require('glob');
 
-module.exports = {
-  context: path.resolve(__dirname),
-  entry: glob.sync('./test/**/*.js'),
-  output: {
-    path: path.resolve(__dirname, '.tmp'),
-    filename: 'test.js'
-  },
-  resolve: {
-    modulesDirectories: ['node_modules'],
-    extensions: ['', '.js', '.json']
-  },
-  watch: true,
-  debug: true,
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.json$/, loader: 'json-loader' }
-    ]
-  }
+const conf = require('./webpack.config');
+
+conf.watch = true;
+conf.debug = true;
+conf.devtool = 'source-map';
+
+conf.context = path.resolve(__dirname);
+conf.entry = glob.sync('./test/**/*.js');
+conf.output = {
+  path: path.resolve(__dirname, '.tmp'),
+  filename: 'test.js'
 };
+
+conf.externals = null;
+
+module.exports = conf;
