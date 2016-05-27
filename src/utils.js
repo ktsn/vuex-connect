067f5848bc3d66d08b1bb3d3ee584d4553c2587e
ktsn <ktsn55@gmail.com>
2016-05-28 00:38:20 +0900

    Add options for injecting lifecycle hooks

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,30 @@ export function camelToKebab(str) {
     .replace(/([a-z\d])([A-Z])/g, '$1-$2')
     .toLowerCase();
 }
+
+export function assign(target, ...args) {
+  args.forEach(obj => {
+    Object.keys(obj).forEach(key => {
+      target[key] = obj[key];
+    });
+  });
+  return target;
+}
+
+export function pick(obj, keys) {
+  const res = {};
+  keys.forEach(key => {
+    if (obj[key] !== void 0) {
+      res[key] = obj[key];
+    }
+  });
+  return res;
+}
+
+export function mapValues(obj, f) {
+  const res = {};
+  Object.keys(obj).forEach(key => {
+    res[key] = f(obj[key], key);
+  });
+  return res;
+}
